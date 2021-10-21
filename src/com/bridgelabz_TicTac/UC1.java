bf9fb04589726d41a11f7f659cbed4c9133507cc
Basila123 <basilanoushad92@gmail.com>
2021-10-21 09:43:25 +0530

    Welcome

---
+++ src/com/bridgelabz_TicTac/UC1.java
@@ -0,0 +1,17 @@
+package com.bridgelabz_TicTac;
+
+public class UC1 {
+
+    public static void main(String arg[]) {
+
+
+        char board[]= new char[10];
+        for (int i=1;i<10;i++){
+
+            board[i]=' ';
+        }
+
+
+    }
+
+}
